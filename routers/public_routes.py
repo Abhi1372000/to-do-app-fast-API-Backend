0ab2a9e04edc8982c5c1357de73e47f14baf7f61
Abhi1372000 <125535785+Abhi1372000@users.noreply.github.com>
2023-11-29 14:02:12 +0530

    some changes in public route

--- routers/public_routes.py
+++ routers/public_routes.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter, Form, HTTPException
+from fastapi import APIRouter, Form
+from responses import responses
 
 public_router = APIRouter()
 
@@ -7,13 +8,12 @@ public_router = APIRouter()
 async def login(username: str = Form(...), password: str = Form(...)):
 
     if not username or not password:
-        error_message = {
-            "status": "login failed",
-            "message": "Both username and password are required"
-        }
-        raise HTTPException(status_code=422, detail=error_message)
-
-    return {"status": "login successfull"}
+        content_res = responses["422"]
+        content_res["message"] = "Both username and password are required"
+        return content_res
+    content_res = responses["200"]
+    content_res["message"] = "login successfull"
+    return content_res
 
 
 @public_router.post("/register")
@@ -21,11 +21,10 @@ async def register(username: str = Form(...), password: str = Form(...),
                    name: str = Form(...), occupation: str = Form(...)):
 
     if not username or not password or not name or not occupation:
-        error_message = {
-            "status": "login failed",
-            "message": "Both username and password are required"
-        }
-        raise HTTPException(status_code=422, detail=error_message)
-    print(username, password, occupation, name)
+        content_res = responses["422"]
+        content_res["message"] = "Both username and password are required"
+        return content_res
 
-    return {"status": "registered successfull"}
+    content_res = responses["200"]
+    content_res["message"] = "registration successfull"
+    return content_res
